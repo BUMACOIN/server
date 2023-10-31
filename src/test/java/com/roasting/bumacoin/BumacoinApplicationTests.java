9e48dca44de5c3f91fc990e52c86a6dde640304c
Ubinquitous <ubinquitous1@gmail.com>
2023-10-31 09:46:40 +0900

    initial commit

---
+++ src/test/java/com/roasting/bumacoin/BumacoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roasting.bumacoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumacoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
